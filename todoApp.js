008720ccfb3006df11afa0d0ecee98f775dac6aa
Joginder Dhaliwal <joginderdhaliwal@Joginders-MacBook-Air.local>
2026-05-12 21:07:55 -0400

    Start CLI todo app

---
+++ todoApp.js
@@ -0,0 +1,24 @@
+let tasks = [
+    { text: "Task1", completed: false },
+    { text: "Task2", completed: false },
+    { text: "Task3", completed: true }
+];
+
+function displayTasks(){
+    tasks.forEach(function(task, index) {
+    let status = "uncompleted";
+    
+    if(task.completed === true){
+        status = "completed";
+    }
+
+    console.log(index + 1 + ". " + task.text + " - " + status);
+    });
+}
+
+function addTask(text){
+    tasks.push({text: text, completed: false});
+}
+
+addTask("Testing");
+displayTasks();
